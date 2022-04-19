b45dc4aa9456136c64d1e6b4ae0803db1cd897d4
Vimal Raj Viswaraj <vimalrajv@maveric-systems.com>
2022-04-20 01:03:37 +0530

    second commit

--- spec/dommanipulations.spec.js
+++ spec/dommanipulations.spec.js
@@ -1,3 +1,4 @@
+
 describe('DomManipulation', ()=>{
     beforeEach(function(){
       const constants = new Constants();
@@ -7,7 +8,40 @@ describe('DomManipulation', ()=>{
 
     it('should be created', ()=>{
         expect(domManipulation).toBeTruthy();
-    });  
+    });
+    
+    describe('addInput',()=>{
+
+      it('should call checkVictory() if symbol and position is passed',()=>{
+        spyOn(ticTacToe,'checkVictory').and.callFake;
+        DomManipulation.addInput("X",[0,0]);
+        expect(ticTacToe.checkVictory()).toHaveBeenCalledTimes(1);
+      })
+
+      //This condition is not handled in the application
+      it('should fail if null symbol is passed as a parameter',()=>{
+        spyOn(ticTacToe,'checkVictory').and.callFake;
+        DomManipulation.addInput(null,[0,0]);
+        expect(ticTacToe.checkVictory()).toHaveBeenCalledTimes(0); 
+        //output would be false as the function would be called once hence there is no check for invalid parameters
+      })
+   
+
+    it('should fail if null position is passed as a parameter',()=>{
+      spyOn(ticTacToe,'checkVictory').and.callFake;
+      DomManipulation.addInput("X",null);
+      expect(ticTacToe.checkVictory()).toHaveBeenCalledTimes(0); 
+      //output would be false as the function would be called once hence there is no check for invalid parameters
+    })
+
+    it('should fail if no parameter is passed to the function',()=>{
+      spyOn(ticTacToe,'checkVictory').and.callFake;
+      DomManipulation.addInput();
+      expect(ticTacToe.checkVictory()).toHaveBeenCalledTimes(0); 
+      //output would be false as the function would be called once hence there is no check for invalid parameters
+    })
+  })
+
 });
   
   
--- spec/tictactoe.spec.js
+++ spec/tictactoe.spec.js
@@ -1,3 +1,5 @@
+import assert from 'assert';
+
 describe('TicTacToe', ()=>{
     beforeEach(function(){
       const constants = new Constants();
@@ -20,6 +22,76 @@ describe('TicTacToe', ()=>{
         ticTacToe.addO([0,0]);
         expect(domManipulation.addInput).toHaveBeenCalledWith('O', [0,0]);
     });
+
+    describe('isClickInGrid',()=>{
+
+        //If Grid values is entered correctlt it shall return true
+        //Assertion statement is used to check the pass creteria
+        it('should return true is a valid position is passed', ()=>{
+            assert.equall(TicTacToe.prototype.isClickInGrid(["grid",2,2]),true);
+        })
+
+        //Assertion to check if it fails when called without a parameter
+        it('should return false if called without a position parameter', ()=>{
+            assert.equall(TicTacToe.prototype.isClickInGrid(),false);
+        })
+
+        //The 2nd condition in the IF loop satisfys and retuns false
+        it('should return false if length of the position is more than 3', ()=>{
+            //assert.equall(TicTacToe.prototype.isClickInGrid(["grid",2,2,2]),false);
+            expect(TicTacToe.prototype.isClickInGrid(["grid",2,2,2])).toBe(false);
+        })
+
+        //The 3rd condition in the IF loop satisfys and retuns false
+        it('should return false is a void position ousite the grid is passed', ()=>{
+            assert.equall(TicTacToe.prototype.isClickInGrid(["form",2,2]),false);
+        })
+
+        //The 4th condition in the IF loop satisfys and retuns false
+        it('should return false is a null value passed in the position index', ()=>{
+            assert.equall(TicTacToe.prototype.isClickInGrid(["grid",null,2]),false);
+        })
+    })
+
+    describe('inputEvent',()=>{
+
+        // calling input function with a null input and validating if the setKeyDone() called or not
+        it('should not call if the parameter passed is null',()=>{
+            spyOn('ticTacToe',setKeyDone()).and.callFake;
+            ticTacToe.setKeyDone(null);
+            expect(ticTacToe.setKeyDone()).toHaveBeenCalledTimes(0);
+        })
+    })
+
+    describe('Xwins',()=>{
+
+        it('should call declaration function in dommanipulation',()=>{
+            spyOn(domManipulation, 'declareVictory').and.callFake;
+            TicTacToe.Xwins();
+            expect(domManipulation.declareVictory).toHaveBeenCalledWith('x');
+        })
+
+    })
+
+    describe('Owins',()=>{
+
+        it('should call declaration function in dommanipulation',()=>{
+            spyOn(domManipulation, 'declareVictory').and.callFake;
+            TicTacToe.Owins();
+            expect(domManipulation.declareVictory).toHaveBeenCalledWith('o');
+        })
+
+    })
+
+    describe('tieWins',()=>{
+
+        it('should call declaration function in dommanipulation',()=>{
+            spyOn(domManipulation, 'declareVictory').and.callFake;
+            TicTacToe.Owins();
+            expect(domManipulation.declareVictory).toHaveBeenCalledWith('tie');
+        })
+
+    })
 });
   
   
